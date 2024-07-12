31e5094e6bb491aaa1f592df2f22e3853c58e6e7
hagar274 <147951304+hagar274@users.noreply.github.com>
2024-07-13 00:58:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Add click event for scroll up button
+scrollBtn = document.querySelector(".scrollBtn");
+window.addEventListener("scroll", ()=>{
+scrollup()
+})
+scrollBtn.addEventListener('click',()=>{
+window.scrollTo({
+top : 0 ,
+behavior : "smooth"
+})
+})
+scrollup()
+function scrollup(){
+window.scrollY > 100 ? scrollBtn.classList.add('active') : scrollBtn.classList.
+remove('active'); 
+}
+
+// slider
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
